d7d1f60ca2bf463859a9a712425f26284efc5bfa
KobESB <kobieroberts@kobies-mbp.mynetworksettings.com>
2024-05-31 01:14:52 -0700

    JavaScript

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+const topics = ["HTML", "CSS", "Git", "JavaScript"];
+
+
+/*if (topics === 'HTML') {
+  console.log("Let's study HTML!");
+  }  else if (topics === 'CSS') {            
+ console.log("Let's study CSS!");
+
+  } else if (topics === 'Git'){
+console.log("Let's study Git!");
+
+  } else if (topics === 'JavaScript') {
+    console.log("Let's study JavavScritp!");
+
+  } else  {
+    console.log('Please try again!');}
+    */
+    for(let i = 0; i < topics.length; i++) {
+        console.log(topics[i]);
+    }
+
+
